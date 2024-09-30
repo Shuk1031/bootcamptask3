b4fe8fef07677774ffb722fda22757771afee125
久保田柊弥 <kubotashuya@kubotahiiragiwatarunoMacBook-Air.local>
2024-09-30 18:18:21 +0900

    Initial commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,22 @@
 // lib/db.ts
+// lib/db.ts
 import { Pool } from 'pg';
 
-let pool: Pool;
+declare global {
+  // グローバルスコープに pool を追加
+  var pool: Pool | undefined;
+}
 
-if (!pool) {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+const pool = global.pool || new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  // 開発環境ではグローバル変数を再利用
+  global.pool = pool;
 }
 
 export default pool;
