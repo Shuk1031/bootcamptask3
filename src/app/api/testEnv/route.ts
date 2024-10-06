4473ca4beab1c1f663b3ba532dfe4cbd0e2b495a
久保田柊弥 <kubotashuya@kubotahiiragiwatarunoMacBook-Air.local>
2024-10-06 22:25:45 +0900

    Initial commit

--- src/app/api/testEnv/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request) {
-  const dbUrl = process.env.DATABASE_URL;
-  return NextResponse.json({ DATABASE_URL: dbUrl });
-}
