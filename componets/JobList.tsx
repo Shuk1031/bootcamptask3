42fed7e647fc29c202142207f41719157e5fe5d0
久保田柊弥 <kubotashuya@kubotahiiragiwatarunoMacBook-Air.local>
2024-10-07 11:21:53 +0900

    Initial commit

--- componets/JobList.tsx
+++ componets/JobList.tsx
@@ -1,34 +1,9 @@
-/*"use client";
-import React from 'react';
-import { Job } from '../types';
+// JobList.tsx
 
-interface JobListProps {
-  jobs: Job[];
-}
-
-const JobList: React.FC<JobListProps> = ({ jobs }) => {
-  return (
-    <div className="p-4 bg-white rounded-lg shadow-lg">
-      <h3 className="text-xl font-bold mb-4 text-gray-800">求人一覧</h3>
-      <ul className="space-y-2">
-        {jobs.map((job) => (
-          <li key={job.id} className="border border-gray-300 p-4 rounded bg-gray-50">
-            <span className="font-semibold">{job.title}</span> | 年収:
-            <span className="text-blue-500"> {job.salary}万円</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default JobList;*/
-// components/JobList.tsx
 "use client";
 
 import React from 'react';
 import { Job } from '../types/types';
-export const dynamic = 'force-dynamic'
 
 interface JobListProps {
   jobs: Job[];
@@ -52,3 +27,32 @@ const JobList: React.FC<JobListProps> = ({ jobs }) => {
 };
 
 export default JobList;
+/*"use client";
+
+import React from 'react';
+import { Job } from '../types/types';
+
+
+
+interface JobListProps {
+  jobs: Job[];
+}
+
+const JobList: React.FC<JobListProps> = ({ jobs }) => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-lg">
+      <h3 className="text-xl font-bold mb-4 text-gray-800">求人一覧</h3>
+      <ul className="space-y-2">
+        {jobs.map((job) => (
+          <li key={job.id} className="border border-gray-300 p-4 rounded bg-gray-50">
+            <span className="font-semibold">{job.title}</span> | 年収:
+            <span className="text-blue-500"> {job.salary}万円</span>   
+            <p className="text-sm text-gray-600">カテゴリ: {job.category}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default JobList;*/
